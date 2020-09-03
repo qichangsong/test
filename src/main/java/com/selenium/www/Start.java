42d46790c3634f8f82c538742bf5c2375dcb0e28
qichangsong <qichangsong@163.com>
2020-09-03 23:06:21 +0800

    添加链接

--- src/main/java/com/selenium/www/Start.java
+++ src/main/java/com/selenium/www/Start.java
@@ -5,16 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Start {
     public static void main(String[] args) {
         String s;
+       String regex="^((http)?:\\/\\/www\\.zuowen\\.com/e)(.*)";
+       String regex1="^((http)?:\\/\\/www\\.zuowen\\.com)(.*)";
         BootChome bootChome=new BootChome();
         WebDriver webDriver=bootChome.bootChrome();
       List<WebElement> list= webDriver.findElements(By.xpath("//a"));
       for(WebElement w:list){
          s= w.getAttribute("href");
-          System.out.println(s);
+        if(s.matches(regex1)){
+             System.out.println(s);
+        }
       }
       webDriver.close();
     }
