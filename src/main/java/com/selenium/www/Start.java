d7133cadf86e77a2f81adca912e84ec5bb9fdad9
qichangsong <qichangsong@163.com>
2020-09-04 11:54:11 +0800

    获取三层链接

--- src/main/java/com/selenium/www/Start.java
+++ src/main/java/com/selenium/www/Start.java
@@ -20,6 +20,10 @@ public class Start {
         ResultSet res;
         List<String> list1=new ArrayList<String>();
         String s;
+        String url;
+        String title;
+        String content;
+        WebElement webElement;
        String regex="^((http)?:\\/\\/www\\.zuowen\\.com/e)(.*)";
        String regex1="^((http)?:\\/\\/www\\.zuowen\\.com)(.*)";
         BootChome bootChome=new BootChome();
@@ -35,9 +39,56 @@ public class Start {
              System.out.println(s);
         }
       }
+      //获取二层连接
          pst=con.prepareStatement("select url from url_save where typecode=1;");
          res=pst.executeQuery();
-
+         list.clear();
+         while (res.next()){
+             url=res.getString("url");
+             webDriver.get(url);
+              list=webDriver.findElements(By.xpath("//a"));
+             for(WebElement w:list){
+                 s= w.getAttribute("href");
+                 if(s.matches(regex1)){
+                     pst=con.prepareStatement("insert into url_save values (?,?)");
+                     pst.setString(1,s);
+                     pst.setInt(2,2);
+                     pst.execute();
+                    // System.out.println(s);
+                 }
+                 list.clear();
+             }
+         }
+         //获取第三层连接
+        pst=con.prepareStatement("select url from url_save where typecode=2;");
+        res=pst.executeQuery();
+        list.clear();
+        while (res.next()){
+            url=res.getString("url");
+            webDriver.get(url);
+            list=webDriver.findElements(By.xpath("//a"));
+            for(WebElement w:list){
+                s= w.getAttribute("href");
+                if(s.matches(regex)){
+                    pst=con.prepareStatement("insert into url_save values (?,?)");
+                    pst.setString(1,s);
+                    pst.setInt(2,3);
+                    pst.execute();
+                    // System.out.println(s);
+                    webDriver.get(s);
+                   webElement= webDriver.findElement(By.className("h_title"));
+                   title=  webElement.getText();
+                   //需要获取div的子元素，还没有处理
+                    webElement= webDriver.findElement(By.className("con_content"));
+                 content=webElement.getAttribute("");
+                 pst=con.prepareStatement("insert into content_save values (?,?,?)");
+                 pst.setString(1,s);
+                 pst.setString(2,title);
+                 pst.setString(3,content);
+                }
+                list.clear();
+            }
+        }
       connect.close();
       webDriver.close();
     }
